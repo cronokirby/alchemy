becbdcaa0331da7e84a57c5c6fc919dc91f66b00
cronokirby <cronokirby@gmail.com>
2017-02-03 21:54:16 +0100

    Initial commit

---
+++ lib/alchemy.ex
@@ -0,0 +1,18 @@
+defmodule Alchemy do
+  @moduledoc """
+  Documentation for Alchemy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Alchemy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alchemy_test.exs
@@ -0,0 +1,8 @@
+defmodule AlchemyTest do
+  use ExUnit.Case
+  doctest Alchemy
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
