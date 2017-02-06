60d7b49001b6b4a964c0ff5472c95d6255a968e6
cronokirby <cronokirby@gmail.com>
2017-02-06 06:38:18 +0100

    Moved public Structs into this folder

---
+++ lib/Structs/user.ex
@@ -0,0 +1,46 @@
+defmodule Alchemy.User do
+  @moduledoc """
+  Represents a discord user. The default values exist to cover missing fields.
+
+  > **id**
+
+    represents a unique client id
+  > **username**
+
+    represents a client's current username
+  > **discriminator**
+
+    4 digit tag to differenciate usernames
+  > **avatar**
+
+    A string representing their avatar hash
+  > **bot**
+
+    Whether or not the user is a bot - *default: `false`*
+
+  A bot usually doesn't have the authorization necessary to access these 2, so
+  they're usually missing.
+  > **verified**
+
+    Whether the account is verified - *default: `:hidden`*
+  > **email**
+
+    The user's email - *default: `:hidden`*
+  """
+  @type t :: %Alchemy.User{id: String.t,
+                           username: String.t,
+                           discriminator: String.t,
+                           avatar: String.t,
+                           bot: Boolean,
+                           verified: :hidden | Boolean,
+                           email: :hidden | String.t}
+  @derive [Poison.Encoder]
+  defstruct [:id,
+             :username,
+             :discriminator,
+             :avatar,
+             bot: false,
+             verified: :hidden,
+             email: :hidden
+             ]
+end
