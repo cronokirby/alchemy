fcfcd4c96da0a26dc0d2b00427f2c6386f8e7256
cronokirby <cronokirby@gmail.com>
2017-02-10 19:09:38 +0100

    Added a supervisor / genserver model to replace an inferior GenEvent model

---
+++ lib/Discord/Events/event_handler.ex
@@ -0,0 +1,18 @@
+defmodule Alchemy.Discord.EventHandler do
+  use GenServer
+  @moduledoc false
+  # A Generic event handler, who's behaviour is specified when it's started
+  def start_link(name, event_type, module, method) do
+    state = %{event_type: event_type, module: module, method: method}
+    GenServer.start_link(__MODULE__, state, name: name)
+  end
+
+  # If the event is of the same type, it calls the stored message
+  def handle_cast({event_type, args}, state) do
+    ev = state.event_type
+    case event_type do
+      ev -> apply(state.module, state.method, args)
+      _ -> nil
+    end
+  end
+end
