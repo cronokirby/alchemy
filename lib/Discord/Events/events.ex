eabea9623b8d70296f07671b0672b649a94175d4
cronokirby <cronokirby@gmail.com>
2017-02-11 15:14:24 +0100

    Added more event handles

--- lib/Discord/Events/events.ex
+++ lib/Discord/Events/events.ex
@@ -1,8 +1,6 @@
 defmodule Alchemy.Discord.Events do
-  alias Alchemy.DMChannel
-  alias Alchemy.Channel
-  alias Alchemy.Guild
-  alias Alchemy.OverWrite
+  alias Alchemy.{Channel, Emoji, DMChannel, Guild, OverWrite, User,
+                 GuildMember, Role}
   alias Alchemy.Discord.StateManager, as: State
   import Alchemy.Structs.Utility
   import Alchemy.Discord.EventManager
@@ -47,4 +45,52 @@ defmodule Alchemy.Discord.Events do
     State.update_guild(guild)
     notify {:guild_update, [Guild.from_map(guild)]}
   end
+
+  # The State is responsible for notifications in this case
+  def handle("GUILD_DELETE", guild) do
+    State.delete(guild)
+  end
+
+  def handle("GUILD_BAN_ADD", %{"guild_id" => id} = user) do
+    notify {:guild_ban, [to_struct(User, user), id]}
+  end
+
+  def handle("GUILD_BAN_REMOVE", %{"guild_id" => id} = user) do
+    notify {:guild_unban, [to_struct(User, user), id]}
+  end
+
+  def handle("GUILD_EMOJIS_UPDATE", data) do
+    State.update_emojis(data)
+    notify {:emoji_update, [map_struct(data["emojis"], Emoji), data["guild_id"]]}
+  end
+
+  def handle("GUILD_INTEGRATIONS_UPDATE", %{"guild_id" => id}) do
+    notify {:integrations_update, [id]}
+  end
+
+  def handle("GUILD_MEMBER_ADD", %{"guild_id" => id}) do
+    notify {:member_join, [id]}
+  end
+
+  def handle("GUILD_MEMBER_REMOVE", %{"guild_id" => id, "user" => user}) do
+    State.remove_user(id, user)
+    notify {:member_leave, [to_struct(user, User), id]}
+  end
+
+  def handle("GUILD_MEMBER_UPDATE", %{"guild_id" => id} = data) do
+    # This key would get popped implicitly later, but I'd rather do it clearly here
+    State.update_member(id, Map.pop(data, "guild_id"))
+    notify {:member_update, [GuildMember.from_mapdata, id]}
+  end
+
+  def handle("GUILD_ROLE_CREATE", %{"guild_id" => id, "role" => role}) do
+    State.add_role(id, role)
+    notify {:role_create, [to_struct(role, Role), id]}
+  end
+
+  def handle("GUILD_ROLE_DELETE", %{"guild_id" => guild_id, "role_id" => id}) do
+    State.remove_role(guild_id, id)
+    notify {:role_delete, [id, guild_id]}
+  end
+
 end
