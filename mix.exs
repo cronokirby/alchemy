36161a59ad1a45c263b761418c8d58bc8425dd75
cronokirby <cronokirby@gmail.com>
2017-02-17 11:26:00 +0100

    v 0.1.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Alchemy.Mixfile do
 
   def project do
     [app: :alchemy,
-     version: "0.1.5",
+     version: "0.1.6",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
