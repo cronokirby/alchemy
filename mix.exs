d19a7723d3637201315717f4e992a2e4cdbd3c16
cronokirby <cronokirby@gmail.com>
2017-02-20 13:42:26 +0100

    removed useless dependancies from the project file

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule Alchemy.Mixfile do
   use Mix.Project
 
+
   def project do
     [app: :alchemy,
      version: "0.1.7",
@@ -10,9 +11,7 @@ defmodule Alchemy.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [mod: {Script, []},
@@ -20,20 +19,10 @@ defmodule Alchemy.Mixfile do
      applications: [:httpotion] ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+
   defp deps do
-    [{:credo, "~> 0.5", only: [:dev, :test]},
-     {:httpotion, "~> 3.0.2"},
+    [{:httpotion, "~> 3.0.2"},
      {:earmark, "~> 0.1", only: :dev},
-     {:socket, "~> 0.3"},
      {:websocket_client, git: "https://github.com/Kraigie/websocket_client.git"},
      {:ex_doc, "~> 0.11", only: :dev},
      {:poison, "~> 3.0"}]
