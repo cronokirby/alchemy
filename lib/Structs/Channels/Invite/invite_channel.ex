77ae2bc4dc528476c200a58ac75d459d293c4f76
cronokirby <cronokirby@gmail.com>
2017-02-24 21:54:51 +0100

    adds an invite channel struct

---
+++ lib/Structs/Channels/Invite/invite_channel.ex
@@ -0,0 +1,27 @@
+defmodule Alchemy.Channel.Invite.InviteChannel do
+  @moduledoc """
+  Represents the channel an invite is for
+
+  - `id`
+
+    The id of the channel
+  - `name`
+
+    The name of the channel
+  - `type`
+
+    the type of the channel, either "text" or "voice"
+  """
+  @type snowflake :: String.t
+
+  @type t :: %__MODULE__{
+    id: snowflake,
+    name: String.t,
+    type: "text" | "voice"
+  }
+  @derive Poison.Encoder
+  defstruct [:id,
+             :name,
+             :splash,
+             :icon]
+end
