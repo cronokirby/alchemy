9d69b678e0f97374625f17395e8ee85f53e5e995
cronokirby <cronokirby@gmail.com>
2017-02-25 01:05:35 +0100

    bump to version 0.1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Alchemy.Mixfile do
 
   def project do
     [app: :alchemy,
-     version: "0.1.7",
+     version: "0.1.8",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
