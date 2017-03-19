7451a5774560448c8663bd51f88b35773a87e27b
cronokirby <cronokirby@gmail.com>
2017-03-19 22:34:19 +0100

    adds a guild integration struct

---
+++ lib/Structs/Guilds/integration.ex
@@ -0,0 +1,32 @@
+defmodule Alchemy.Guild.Integration do
+  @moduledoc false
+  alias Alchemy.User
+  import Alchemy.Structs
+
+
+  defstruct [:id,
+             :name,
+             :type,
+             :enabled,
+             :syncing,
+             :role_id,
+             :expire_behaviour,
+             :expire_grace_period,
+             :user,
+             :account,
+             :synced_at]
+
+
+  defmodule Account do
+      @moduledoc false
+      defstruct [:id, :name]
+  end
+
+
+  def from_map(map) do
+    map
+    |> field("user", User)
+    |> field("account", Account)
+    |> to_struct(__MODULE__)
+  end
+end
