7aa2d3166618f82b06a00b780d0646e5b1a24e4b
cronokirby <cronokirby@gmail.com>
2017-03-21 23:07:15 +0100

    adds basic functions for interacting with the cache

--- lib/cache.ex
+++ lib/cache.ex
@@ -9,7 +9,7 @@ defmodule Alchemy.Cache do
   to get information from the context of commands.
   """
   alias Alchemy.Cache.Guilds
-  alias Alchemy.{Guild, GuildMember}
+  alias Alchemy.{Emoji, Guild, GuildMember, Role, Users.Presence}
 
 
   @type snowflake :: String.t
@@ -22,11 +22,11 @@ defmodule Alchemy.Cache do
   need for getting the whole struct. Because of how the registry is set up, getting
   the entire guild requires a whole extra step, that passes through this one anyways.
   """
-  @spec guild_id(snowflake) :: {:some, snowflake} | :none
+  @spec guild_id(snowflake) :: {:ok, snowflake} | {:error, String.t}
   def guild_id(channel_id) do
     case :ets.lookup(:channels, channel_id) do
-       [{_, id}] -> {:some, id}
-       [] -> :none
+       [{_, id}] -> {:ok, id}
+       [] ->  {:error, "Failed to find a channel entry for #{channel_id}."}
     end
   end
   @doc """
@@ -42,11 +42,12 @@ defmodule Alchemy.Cache do
   - `channel`
     Using this keyword will fetch the information for the guild a channel belongs to.
   """
-  @spec guild(snowflake) :: {:some, Guild.t} | :none
+  @spec guild(snowflake) :: {:ok, Guild.t} | {:error, String.t}
   def guild(guild_id) do
-    Guilds.call(guild_id, :show)
+    g = Guilds.call(guild_id, :show)
     |> Guilds.de_index
     |> Guild.from_map
+    {:ok, g}
   end
   def guild(channel: channel_id) do
     with {:some, id} <- guild_id(channel_id) do
@@ -62,18 +63,39 @@ defmodule Alchemy.Cache do
       |> get_in([id])
     case maybe_val do
       nil ->
-        :none
+        {:error, "Failed to find an entry for #{id} in section #{section}"}
       some ->
-        {:some, module.from_map(some)}
+        {:ok, module.from_map(some)}
     end
   end
   @doc """
   Gets a member from a cache, by guild and member id.
   """
-  @spec member(snowflake, snowflake) :: {:some, GuildMember.t} | :none
+  @spec member(snowflake, snowflake) :: {:ok, GuildMember.t} | {:error, String.t}
   def member(guild_id, member_id) do
     access(guild_id, "members", member_id, GuildMember)
   end
+  @doc """
+  Gets a specific role in a guild.
+  """
+  @spec role(snowflake, snowflake) :: {:ok, Role.t} | {:error, String.t}
+  def role(guild_id, role_id) do
+    access(guild_id, "roles", role_id, Role)
+  end
+  @doc """
+  Gets the presence of a user in a certain guild.
 
-
+  This contains info such as their status, and roles.
+  """
+  @spec presence(snowflake, snowflake) :: {:some, Presence.t} | {:error, String.t}
+  def presence(guild_id, user_id) do
+    access(guild_id, "presences", user_id, Presence)
+  end
+  @doc """
+  Retrieves a custom emoji by id in a guild.
+  """
+  @spec emoji(snowflake, snowflake) :: {:some, Emoji.t} | {:error, String.t}
+  def emoji(guild_id, emoji_id) do
+    access(guild_id, "emojis", emoji_id, Emoji)
+  end
 end
