ad944848dbce9427cbd26209c22c89ac55d4e263
cronokirby <cronokirby@gmail.com>
2017-03-27 07:34:00 +0200

    adds a basic webhook struct

--- lib/Structs/webhooks.ex
+++ lib/Structs/webhooks.ex
@@ -1,6 +1,22 @@
 defmodule Alchemy.Webhooks do
   @moduledoc """
   """
+  alias Alchemy.User
+
+
+  @type snowflake :: String.t
+
+
+  @type t :: %__MODULE__{
+    id: snowflake,
+    guild_id: snowflake | nil,
+    channel_id: snowflake,
+    user: User.t | nil,
+    name: String.t | nil,
+    avatar: String.t | nil,
+    token: String.t
+  }
+
 
   defstruct [:id,
              :guild_id,
@@ -9,4 +25,5 @@ defmodule Alchemy.Webhooks do
              :name,
              :avatar,
              :token]
+
 end
