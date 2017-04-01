d137091bd8ef565465651b774e3625e5fdbc9668
cronokirby <cronokirby@gmail.com>
2017-04-01 18:26:07 +0200

    adds documentation for the Message module

--- lib/Structs/Messages/message.ex
+++ lib/Structs/Messages/message.ex
@@ -2,16 +2,55 @@ defmodule Alchemy.Message do
   import Alchemy.Structs
   alias Alchemy.{User, Attachment, Embed, Reaction}
   @moduledoc """
+  This module contains the types and functions related to messages in discord.
   """
 
   @type snowflake :: String.t
+  @typedoc """
+  Represents an iso8601 timestamp.
+  """
+  @type timestamp :: String.t
+  @typedoc """
+  Represents a message in a channel.
 
+  - `id`
+    The id of this message.
+  - `author`
+    The user who sent this message. This field will be very incomplete
+    if the message originated from a webhook.
+  - `content`
+    The content of the message.
+  - `timestamp`
+    The timestamp of the message.
+  - `edit_timestamp`
+    The timestamp of when this message was edited, if it ever was.
+  - `tts`
+    Whether this was a tts message.
+  - `mention_everyone`
+    Whether this message mentioned everyone.
+  - `mentions`
+    A list of users this message mentioned.
+  - `mention_roles`
+    A list of role ids this message mentioned.
+  - `attachments`
+    A list of attachments to the message.
+  - `embeds`
+    A list of embeds attached to this message.
+  - `reactions`
+    A list of reactions to this message.
+  - `nonce`
+    Used for validating a message was sent.
+  - `pinned`
+    Whether this message is pinned.
+  - `webhook_id`
+    The id of the webhook that sent this message, if it was sent by a webhook.
+  """
   @type t :: %__MODULE__{
     id: snowflake,
     channel_id: snowflake,
     author: User.t,
     content: String,
-    timestamp: String,
+    timestamp: timestamp,
     edited_timestamp: String | nil,
     tts: Boolean,
     mention_everyone: Boolean,
@@ -20,7 +59,7 @@ defmodule Alchemy.Message do
     attachments: [Attachment.t],
     embeds: [Embed.t],
     reactions: [Reaction.t],
-    nonce: String.t,
+    nonce: snowflake,
     pinned: Boolean,
     webhook_id: String.t | nil
   }
