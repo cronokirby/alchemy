16b0b67754481c2893cd0fbf92ea89e0781baa22
cronokirby <cronokirby@gmail.com>
2017-04-13 16:55:06 +0200

    upgrades version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Alchemy.Mixfile do
 
   def project do
     [app: :alchemy,
-     version: "0.2.2",
+     version: "0.3.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
