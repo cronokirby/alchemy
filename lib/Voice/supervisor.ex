8ef32f075553299c1d8d02f94c35595924589713
cronokirby <cronokirby@gmail.com>
2017-06-23 13:18:29 +0200

    adds a test for the basic server

--- lib/Voice/supervisor.ex
+++ lib/Voice/supervisor.ex
@@ -10,15 +10,35 @@ defmodule Alchemy.Voice.Supervisor do
 
   def init(:ok) do
     children = [
-      (supervisor(Registry, [:unique, __MODULE__.Registry]))
+      supervisor(Registry, [:unique, __MODULE__.Registry]),
+      worker(__MODULE__.Server, [])
     ]
 
     supervise(children, strategy: :one_for_one)
   end
 
   defmodule Registry do
+    @moduledoc false
     def via(key) do
       {:via, Registry, {__MODULE__, key}}
+    end
+  end
+
+  defmodule Server do
+    @moduledoc false
+    use GenServer
+
+    def start_link do
+      GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+    end
+
+    def handle_call({:start_client, guild}, {pid, _}, state) do
+      case Map.get(state, guild) do
+        nil ->
+          {:reply, :ok, Map.put(state, guild, pid)}
+        _ ->
+          {:reply, {:error, "Already joining this guild"}, state}
+      end
     end
   end
 end
