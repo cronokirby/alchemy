9f2ee9c97088854525ea3cf5188b03c62a418f91
Lúcás Meier <cronokirby@gmail.com>
2018-01-21 22:17:32 +0100

    adds a basic text_channel struct

---
+++ lib/Structs/Channels/text_channel.ex
@@ -0,0 +1,23 @@
+defmodule Alchemy.Channel.TextChannel do
+  @moduledoc false
+  alias Alchemy.OverWrite
+  import Alchemy.Structs
+
+  defstruct [:id,
+             :guild_id,
+             :position,
+             :permission_overwrites,
+             :name,
+             :topic,
+             :nswf,
+             :last_message_id,
+             :icon,
+             :parent_id,
+             :last_pin_timestamp]
+
+  def from_map(map) do
+    map
+    |> field_map("permission_overwrites", &(map_struct &1, OverWrite))
+    |> to_struct(__MODULE__)
+  end
+end
