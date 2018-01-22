d07b1a02c1a466879090d76171fa0935f3cc372e
Lúcás Meier <cronokirby@gmail.com>
2018-01-22 14:04:51 +0100

    adds a group_dm_channel struct

--- lib/Structs/Channels/group_dm_channel.ex
+++ lib/Structs/Channels/group_dm_channel.ex
@@ -1,28 +1,16 @@
-defmodule Alchemy.Channel.DMChannel do
+defmodule Alchemy.Channel.GroupDMChannel do
   @moduledoc false
   alias Alchemy.User
   import Alchemy.Structs
 
-
-  @derive [Poison.Encoder]
   defstruct [:id,
+             :owner_id,
+             :icon,
+             :name,
              :recipients,
              :last_message_id
             ]
-
-
-  @doc false
-  def channel_type(code) do
-    case code do
-      0 -> :text
-      1 -> :private
-      2 -> :voice
-      3 -> :group
-      4 -> :guild_category
-    end
-  end
-
-
+  
   def from_map(map) do
     map
     |> field_map("recipients", &map_struct(&1, User))
