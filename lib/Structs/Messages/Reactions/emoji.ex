2964916c5968b82b0824d36c269081a00dace062
Aram Menocci <aramsm3@gmail.com>
2020-09-22 21:33:53 -0300

    [emoji] added map cases into resolve/1

--- lib/Structs/Messages/Reactions/emoji.ex
+++ lib/Structs/Messages/Reactions/emoji.ex
@@ -5,11 +5,34 @@ defmodule Alchemy.Reaction.Emoji do
   defstruct [:id,
              :name]
 
+  @type t :: %__MODULE__{id: String.t(), name: String.t()}
 
-  @doc false
+
+  @doc """
+    Returns the %Emoji{} struct,
+    resolving its values according with the data type of the parameter `emoji`.
+
+    ## Example
+    ```shell
+    iex(1)> Alchemy.Reaction.Emoji.resolve(%Emoji{id: nil, name: "✅"})
+    %Emoji{id: nil, name: "✅"}
+
+    iex(2)> Alchemy.Reaction.Emoji.resolve(%{id: nil, name: "✅"})
+    %Emoji{id: nil, name: "✅"}
+
+    iex(3)> Alchemy.Reaction.Emoji.resolve(%{"id" => nil, "name" => "✅"})
+    %Emoji{id: nil, name: "✅"}
+
+    iex(4)> Alchemy.Reaction.Emoji.resolve("✅")
+    %Emoji{id: nil, name: "✅"}
+    ```
+  """
+  @spec resolve(emoji :: any()) :: Emoji.t()
   def resolve(emoji) do
     case emoji do
       %__MODULE__{} = em -> em
+      %{"id" => id, "name" => name} -> %__MODULE__{id: id, name: name}
+      %{id: id, name: name} -> %__MODULE__{id: id, name: name}
       unicode -> %__MODULE__{name: unicode}
     end
   end
