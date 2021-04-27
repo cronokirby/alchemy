a11e1198d64ad759be5c5ac10334867fca50ce38
Zarathustra2 <dario.heinisch@gmail.com>
2021-04-27 18:08:24 +0200

    FEAT: Added message reference

--- lib/Structs/Messages/message_reference.ex
+++ lib/Structs/Messages/message_reference.ex
@@ -1,5 +1,19 @@
 defmodule Alchemy.MessageReference do
-  @moduledoc false
+  @moduledoc """
+  Represents a reply to a discord message.
+
+  To reply with the bot to a message use it as following:
+
+  ## Examples
+  ```elixir
+  m = %Alchemy.MessageReference{
+     # ID of the message you would like to reply to
+     message_id: message_id,
+     guild_id: guild_id
+  }
+  Client.send_message(channel_id, "Reply", message_reference: m)
+  ```
+  """
 
   @derive Poison.Encoder
   defstruct [
