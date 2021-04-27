13a0dd5a014190b5d4fa00f02e7910fb9c1ad8a4
Lúcás Meier <cronokirby@gmail.com>
2021-04-27 18:13:12 +0200

    Version 0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Alchemy.Mixfile do
   def project do
     [
       app: :alchemy,
-      version: "0.6.10",
+      version: "0.7.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
